7aa9740c0fa517ddc09060746f5390fb8ace8989
Gopi <hi_gops@hotmail.com>
2025-05-31 17:49:32 -0500

    Added new lines in the samplefile1

--- src/Samplefile1.java
+++ src/Samplefile1.java
@@ -3,5 +3,10 @@ public class Samplefile1 {
         System.out.println("Hi 1");
         System.out.println("Hi 2");
         System.out.println("Hi 3");
+
+        //Adding more line to add in the NewFeature1 branch
+        System.out.println("New line1 added to newfeature1 branch");
+        System.out.println("New line2 added to newfeature1 branch");
+        System.out.println("New line3 added to newfeature1 branch");
     }
 }
