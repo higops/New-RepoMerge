9b0a855b23ab927c72f5466345588383e4fe1830
Gopi <hi_gops@hotmail.com>
2025-05-31 17:43:33 -0500

    Commit1 sample file and git ignore

---
+++ src/Samplefile1.java
@@ -0,0 +1,7 @@
+public class Samplefile1 {
+    public static void main(String[] args) {
+        System.out.println("Hi 1");
+        System.out.println("Hi 2");
+        System.out.println("Hi 3");
+    }
+}
