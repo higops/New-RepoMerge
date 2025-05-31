97e0330f5e0e4df065a02c519e9356ca710f2d78
Gopi <hi_gops@hotmail.com>
2025-05-31 17:58:58 -0500

    Added 2nd file name varaible to master file and 1 line added to master file

--- src/Samplefile1.java
+++ src/Samplefile1.java
@@ -3,5 +3,8 @@ public class Samplefile1 {
         System.out.println("Hi 1");
         System.out.println("Hi 2");
         System.out.println("Hi 3");
+
+        //Adding 1 more line to this file in master branch
+        System.out.println("Added 1 line");
     }
 }
---
+++ src/Samplevariable1.java
@@ -0,0 +1,9 @@
+public class Samplevariable1 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+
+        System.out.println("The variables added");
+    }
+}
