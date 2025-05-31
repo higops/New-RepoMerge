50e9db27667843e2fc3d6f8914ad556d36a01185
Gopi <hi_gops@hotmail.com>
2025-05-31 18:28:29 -0500

    Adding string file in newfeatue branch

---
+++ src/Stringfile1.java
@@ -0,0 +1,9 @@
+public class Stringfile1 {
+    public static void main(String[] args) {
+        String a="Hi Gopi";
+        String b="Hi Gops";
+
+        System.out.println("The string both is : " + (a+b));
+    }
+
+}
