61a894a9c0b437bd24140f7dbb61b185cbde00e2
Gopi <hi_gops@hotmail.com>
2025-05-31 21:37:16 -0500

    Commit for gitclone/pull file ex

---
+++ src/Sampleclonefile.java
@@ -0,0 +1,6 @@
+public class Sampleclonefile {
+    public static void main(String[] args) {
+        //This is sample file pushed to git hub then pull to local copy
+        System.out.println("Hi");
+    }
+}
